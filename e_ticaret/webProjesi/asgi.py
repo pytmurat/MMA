9c74d6a097b8fb5a8360d21a8b6668b276d3493b
pytmurat <75987719+pytmurat@users.noreply.github.com>
2021-03-17 15:15:50 +0300

    A

---
+++ e_ticaret/webProjesi/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for webProjesi project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webProjesi.settings')
+
+application = get_asgi_application()
